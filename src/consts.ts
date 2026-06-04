3b90fb9b9b2d7caeafbbe7801333596e7c874f15
qg <qg@mac.attlocal.net>
2026-06-03 23:24:49 -0700

    minor update

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,3 @@
-export const SITE_TITLE = 'Blake Guo';
+export const SITE_TITLE = "Guo's Blog";
 export const SITE_DESCRIPTION =
 	'Durable writing on ML/AI, distributed systems, and data infrastructure.';
